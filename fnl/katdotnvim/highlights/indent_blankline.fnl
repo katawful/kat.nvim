bde8b5226587ddd9dc2c325cf8e56dd426de985c
katawful <katisntgood@gmail.com>
2021-10-24 12:38:56 -0400

    [indentBlankline] Added support

---
+++ fnl/katdotnvim/highlights/indent_blankline.fnl
@@ -0,0 +1,13 @@
+(module katdotnvim.highlights.indent_blankline
+        {autoload {ucolors katdotnvim.utils.color
+                   colors katdotnvim.color
+                   syntax katdotnvim.highlights.syntax
+                   groups katdotnvim.highlights.main
+                   }})
+
+(defn init []
+  (ucolors.highlight :IndentBlanklineChar groups.shadowBG :SKIP :nocombine)
+  (ucolors.highlight :IndentBlanklineSpaceChar groups.shadowBG :SKIP :nocombine)
+  (ucolors.highlight :IndentBlanklineSpaceChar groups.shadowBG :SKIP :nocombine)
+  (ucolors.highlight :IndentBlanklineContextChar groups.selectionBG :SKIP :nocombine)
+  )
---
+++ lua/katdotnvim/highlights/indent_blankline.lua
@@ -0,0 +1,25 @@
+local _2afile_2a = "fnl/katdotnvim/highlights/indent_blankline.fnl"
+local _2amodule_name_2a = "katdotnvim.highlights.indent_blankline"
+local _2amodule_2a
+do
+  package.loaded[_2amodule_name_2a] = {}
+  _2amodule_2a = package.loaded[_2amodule_name_2a]
+end
+local _2amodule_locals_2a
+do
+  _2amodule_2a["aniseed/locals"] = {}
+  _2amodule_locals_2a = (_2amodule_2a)["aniseed/locals"]
+end
+local autoload = (require("katdotnvim.aniseed.autoload")).autoload
+local colors, groups, syntax, ucolors = autoload("katdotnvim.color"), autoload("katdotnvim.highlights.main"), autoload("katdotnvim.highlights.syntax"), autoload("katdotnvim.utils.color")
+do end (_2amodule_locals_2a)["colors"] = colors
+_2amodule_locals_2a["groups"] = groups
+_2amodule_locals_2a["syntax"] = syntax
+_2amodule_locals_2a["ucolors"] = ucolors
+local function init()
+  ucolors.highlight("IndentBlanklineChar", groups.shadowBG, "SKIP", "nocombine")
+  ucolors.highlight("IndentBlanklineSpaceChar", groups.shadowBG, "SKIP", "nocombine")
+  ucolors.highlight("IndentBlanklineSpaceChar", groups.shadowBG, "SKIP", "nocombine")
+  return ucolors.highlight("IndentBlanklineContextChar", groups.selectionBG, "SKIP", "nocombine")
+end
+_2amodule_2a["init"] = init
