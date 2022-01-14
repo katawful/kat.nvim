5a9339b79abe2d1bba93b8c84c0944aa061ff721
katawful <katisntgood@gmail.com>
2022-01-14 18:30:41 -0500

    [Dev] Updated aniseed

--- lua/katdotnvim/aniseed/deps/fennel.lua
+++ lua/katdotnvim/aniseed/deps/fennel.lua
@@ -363,10 +363,10 @@ package.preload["katdotnvim.aniseed.fennel.repl"] = package.preload["katdotnvim.
           _571_ = _572_
         end
       end
-      if ((_G.type(_571_) == "table") and (nil ~= (_571_).source) and ((_571_).what == "Lua") and (nil ~= (_571_).linedefined) and (nil ~= (_571_).short_src)) then
-        local source = (_571_).source
-        local line = (_571_).linedefined
+      if ((_G.type(_571_) == "table") and (nil ~= (_571_).short_src) and (nil ~= (_571_).linedefined) and ((_571_).what == "Lua") and (nil ~= (_571_).source)) then
         local src = (_571_).short_src
+        local line = (_571_).linedefined
+        local source = (_571_).source
         local fnlsrc
         do
           local t_576_ = compiler.sourcemap
@@ -4150,7 +4150,7 @@ package.preload["katdotnvim.aniseed.fennel.view"] = package.preload["katdotnvim.
 end
 package.preload["katdotnvim.aniseed.fennel.utils"] = package.preload["katdotnvim.aniseed.fennel.utils"] or function(...)
   local view = require("katdotnvim.aniseed.fennel.view")
-  local version = "1.0.0-dev"
+  local version = "1.0.0"
   local function warn(message)
     if (_G.io and _G.io.stderr) then
       return (_G.io.stderr):write(("--WARNING: %s\n"):format(tostring(message)))
