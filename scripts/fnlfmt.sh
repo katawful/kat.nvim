5df2f6bc0bc4dab92bb11ff454501f47cc51da92
katawful <katisntgood@gmail.com>
2022-09-25 12:37:21 -0400

    [fnlfmt] allow formatting of testing dir

--- scripts/fnlfmt.sh
+++ scripts/fnlfmt.sh
@@ -3,4 +3,7 @@
 for i in $(find 'fnl/katdotnvim/' -name "*.fnl"); do
   fnlfmt --fix "$i"
 done;
+for i in $(find 'test/fnl/katdotnvim/' -name "*.fnl"); do
+  fnlfmt --fix "$i"
+done;
 echo "Done formatting with fnlfmt"
