5c830332f3b108bf5df8990697c56487a68e871d
katawful <katisntgood@gmail.com>
2022-10-08 16:40:45 -0400

    [highlights.terminal] fix compilation

--- fnl/katdotnvim/highlights/terminal.fnl
+++ fnl/katdotnvim/highlights/terminal.fnl
@@ -1,35 +1,35 @@
 (module katdotnvim.highlights.terminal
-        {autoload {colors katdotnvim.color
-                   ucolors katdotnvim.utils.highlight.utils
-                   main katdotnvim.main}
+        {require {color-table katdotnvim.color
+                  ucolors katdotnvim.utils.highlight.utils
+                  main katdotnvim.main}
          require-macros [katcros-fnl.macros.nvim.api.options.macros]})
 
 ; this affects the terminal
 
 (defn init []
       (set-vars g
-                {:terminal_color_0 colors.kat.bg.base.color
-                 :terminal_color_1 colors.kat.red.base.color
-                 :terminal_color_2 colors.kat.green.base.color
-                 :terminal_color_3 colors.kat.orange.base.color
-                 :terminal_color_4 colors.kat.blue.base.color
-                 :terminal_color_5 colors.kat.pink.base.color
-                 :terminal_color_6 colors.kat.purple.base.color
-                 :terminal_color_7 colors.kat.fg.base.color
-                 :terminal_color_8 colors.kat.bg.umbra.color
-                 :terminal_color_15 colors.kat.fg.umbra.color})
+                {:terminal_color_0 color-table.kat.bg.base.color
+                 :terminal_color_1 color-table.kat.red.base.color
+                 :terminal_color_2 color-table.kat.green.base.color
+                 :terminal_color_3 color-table.kat.orange.base.color
+                 :terminal_color_4 color-table.kat.blue.base.color
+                 :terminal_color_5 color-table.kat.pink.base.color
+                 :terminal_color_6 color-table.kat.purple.base.color
+                 :terminal_color_7 color-table.kat.fg.base.color
+                 :terminal_color_8 color-table.kat.bg.umbra.color
+                 :terminal_color_15 color-table.kat.fg.umbra.color})
       (if (= (. main.background-mut 1) :light)
           (set-vars g
-                    {:terminal_color_9 (ucolors.darken colors.kat.red.base.color 0.2)
-                     :terminal_color_10 (ucolors.darken colors.kat.green.base.color 0.2)
-                     :terminal_color_11 (ucolors.darken colors.kat.orange.base.color 0.2)
-                     :terminal_color_12 (ucolors.darken colors.kat.blue.base.color 0.2)
-                     :terminal_color_13 (ucolors.darken colors.kat.pink.base.color 0.2)
-                     :terminal_color_14 (ucolors.darken colors.kat.purple.base.color 0.2)})
+                    {:terminal_color_9 (ucolors.darken color-table.kat.red.base.color 0.2)
+                     :terminal_color_10 (ucolors.darken color-table.kat.green.base.color 0.2)
+                     :terminal_color_11 (ucolors.darken color-table.kat.orange.base.color 0.2)
+                     :terminal_color_12 (ucolors.darken color-table.kat.blue.base.color 0.2)
+                     :terminal_color_13 (ucolors.darken color-table.kat.pink.base.color 0.2)
+                     :terminal_color_14 (ucolors.darken color-table.kat.purple.base.color 0.2)})
           (set-vars g
-                    {:terminal_color_9 (ucolors.brighten colors.kat.red.base.color 0.2)
-                     :terminal_color_10 (ucolors.brighten colors.kat.green.base.color 0.2)
-                     :terminal_color_11 (ucolors.brighten colors.kat.orange.base.color 0.2)
-                     :terminal_color_12 (ucolors.brighten colors.kat.blue.base.color 0.2)
-                     :terminal_color_13 (ucolors.brighten colors.kat.pink.base.color 0.2)
-                     :terminal_color_14 (ucolors.brighten colors.kat.purple.base.color 0.2)})))
+                    {:terminal_color_9 (ucolors.brighten color-table.kat.red.base.color 0.2)
+                     :terminal_color_10 (ucolors.brighten color-table.kat.green.base.color 0.2)
+                     :terminal_color_11 (ucolors.brighten color-table.kat.orange.base.color 0.2)
+                     :terminal_color_12 (ucolors.brighten color-table.kat.blue.base.color 0.2)
+                     :terminal_color_13 (ucolors.brighten color-table.kat.pink.base.color 0.2)
+                     :terminal_color_14 (ucolors.brighten color-table.kat.purple.base.color 0.2)})))
--- lua/katdotnvim/highlights/terminal.lua
+++ lua/katdotnvim/highlights/terminal.lua
@@ -10,39 +10,38 @@ do
   _2amodule_2a["aniseed/locals"] = {}
   _2amodule_locals_2a = (_2amodule_2a)["aniseed/locals"]
 end
-local autoload = (require("katdotnvim.aniseed.autoload")).autoload
-local colors, main, ucolors, _ = autoload("katdotnvim.color"), autoload("katdotnvim.main"), autoload("katdotnvim.utils.highlight.utils"), nil
-_2amodule_locals_2a["colors"] = colors
+local color_table, main, ucolors, _ = require("katdotnvim.color"), require("katdotnvim.main"), require("katdotnvim.utils.highlight.utils"), nil
+_2amodule_locals_2a["color-table"] = color_table
 _2amodule_locals_2a["main"] = main
 _2amodule_locals_2a["ucolors"] = ucolors
 _2amodule_locals_2a["_"] = _
 local function init()
-  vim.g["terminal_color_7"] = colors.kat.fg.base.color
-  vim.g["terminal_color_8"] = colors.kat.bg.umbra.color
-  vim.g["terminal_color_0"] = colors.kat.bg.base.color
-  vim.g["terminal_color_1"] = colors.kat.red.base.color
-  vim.g["terminal_color_3"] = colors.kat.orange.base.color
-  vim.g["terminal_color_4"] = colors.kat.blue.base.color
-  vim.g["terminal_color_15"] = colors.kat.fg.umbra.color
-  vim.g["terminal_color_5"] = colors.kat.pink.base.color
-  vim.g["terminal_color_2"] = colors.kat.green.base.color
-  vim.g["terminal_color_6"] = colors.kat.purple.base.color
+  vim.g["terminal_color_15"] = color_table.kat.fg.umbra.color
+  vim.g["terminal_color_4"] = color_table.kat.blue.base.color
+  vim.g["terminal_color_5"] = color_table.kat.pink.base.color
+  vim.g["terminal_color_0"] = color_table.kat.bg.base.color
+  vim.g["terminal_color_6"] = color_table.kat.purple.base.color
+  vim.g["terminal_color_1"] = color_table.kat.red.base.color
+  vim.g["terminal_color_7"] = color_table.kat.fg.base.color
+  vim.g["terminal_color_2"] = color_table.kat.green.base.color
+  vim.g["terminal_color_8"] = color_table.kat.bg.umbra.color
+  vim.g["terminal_color_3"] = color_table.kat.orange.base.color
   do local _ = {nil, nil, nil, nil, nil, nil, nil, nil, nil, nil} end
   if ((main["background-mut"])[1] == "light") then
-    vim.g["terminal_color_9"] = ucolors.darken(colors.kat.red.base.color, 0.2)
-    do end (vim.g)["terminal_color_13"] = ucolors.darken(colors.kat.pink.base.color, 0.2)
-    do end (vim.g)["terminal_color_10"] = ucolors.darken(colors.kat.green.base.color, 0.2)
-    do end (vim.g)["terminal_color_11"] = ucolors.darken(colors.kat.orange.base.color, 0.2)
-    do end (vim.g)["terminal_color_14"] = ucolors.darken(colors.kat.purple.base.color, 0.2)
-    do end (vim.g)["terminal_color_12"] = ucolors.darken(colors.kat.blue.base.color, 0.2)
+    vim.g["terminal_color_13"] = ucolors.darken(color_table.kat.pink.base.color, 0.2)
+    do end (vim.g)["terminal_color_10"] = ucolors.darken(color_table.kat.green.base.color, 0.2)
+    do end (vim.g)["terminal_color_14"] = ucolors.darken(color_table.kat.purple.base.color, 0.2)
+    do end (vim.g)["terminal_color_11"] = ucolors.darken(color_table.kat.orange.base.color, 0.2)
+    do end (vim.g)["terminal_color_9"] = ucolors.darken(color_table.kat.red.base.color, 0.2)
+    do end (vim.g)["terminal_color_12"] = ucolors.darken(color_table.kat.blue.base.color, 0.2)
     return {nil, nil, nil, nil, nil, nil}
   else
-    vim.g["terminal_color_9"] = ucolors.brighten(colors.kat.red.base.color, 0.2)
-    do end (vim.g)["terminal_color_13"] = ucolors.brighten(colors.kat.pink.base.color, 0.2)
-    do end (vim.g)["terminal_color_10"] = ucolors.brighten(colors.kat.green.base.color, 0.2)
-    do end (vim.g)["terminal_color_11"] = ucolors.brighten(colors.kat.orange.base.color, 0.2)
-    do end (vim.g)["terminal_color_14"] = ucolors.brighten(colors.kat.purple.base.color, 0.2)
-    do end (vim.g)["terminal_color_12"] = ucolors.brighten(colors.kat.blue.base.color, 0.2)
+    vim.g["terminal_color_13"] = ucolors.brighten(color_table.kat.pink.base.color, 0.2)
+    do end (vim.g)["terminal_color_10"] = ucolors.brighten(color_table.kat.green.base.color, 0.2)
+    do end (vim.g)["terminal_color_14"] = ucolors.brighten(color_table.kat.purple.base.color, 0.2)
+    do end (vim.g)["terminal_color_11"] = ucolors.brighten(color_table.kat.orange.base.color, 0.2)
+    do end (vim.g)["terminal_color_9"] = ucolors.brighten(color_table.kat.red.base.color, 0.2)
+    do end (vim.g)["terminal_color_12"] = ucolors.brighten(color_table.kat.blue.base.color, 0.2)
     return {nil, nil, nil, nil, nil, nil}
   end
 end
